c625d1c1d4f90eb08499df9cf1214ab4870e2e48
hieunm54 <hieunm.542@gmail.com>
2023-12-27 18:00:03 +0700

    implement logic for comments and posts

---
+++ controllers/Upvote.js
@@ -0,0 +1,46 @@
+import Post from '../models/Post.js';
+import { httpStatus } from '../utils/httpStatus.js';
+
+export const vote = async (req, res) => {
+  try {
+    const userID = req.userID;
+    const { postID } = req.params;
+    const post = await Post.findById(postID).populate({
+      path: 'upvote',
+      select: '_id',
+    });
+    if (post == null) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: 'Can not find post' });
+    }
+
+    // check if userID is in list of upvote
+    const upvoteList = [...post.upvote];
+    const foundUpvoteIndex = upvoteList.findIndex(
+      (vote) => vote._id.toString() === userID,
+    );
+    if (foundUpvoteIndex !== -1) {
+      upvoteList.splice(foundUpvoteIndex, 1);
+    } else {
+      upvoteList.push({ _id: userID });
+    }
+
+    // Update post vote
+    const updatedPost = await Post.findByIdAndUpdate(
+      postID,
+      {
+        $set: {
+          upvote: upvoteList,
+        },
+      },
+      { new: true },
+    );
+
+    res
+      .status(httpStatus.CREATED)
+      .json({ post: updatedPost, upvote: upvoteList });
+  } catch (error) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ error: error.message });
+  }
+};
--- routes/Post.js
+++ routes/Post.js
@@ -9,6 +9,14 @@ import {
   updatePost,
 } from '../controllers/Post.js';
 
+import {
+  createComment,
+  deleteComment,
+  getAllComments,
+  updateComment,
+} from '../controllers/PostComment.js';
+import { vote } from '../controllers/Upvote.js';
+
 const postRoutes = express.Router();
 
 postRoutes.get('/', isAuth, fetchAllPosts);
@@ -16,5 +24,14 @@ postRoutes.get('/:id', isAuth, getPostDetail);
 postRoutes.post('/', isAuth, createPost);
 postRoutes.put('/:id', isAuth, updatePost);
 postRoutes.delete('/:id', isAuth, deletePost);
+
+// Routes for comments
+postRoutes.get('/:postID/comments', isAuth, getAllComments);
+postRoutes.post('/:postID/comments', isAuth, createComment);
+postRoutes.put('/:postID/comments/:id', isAuth, updateComment);
+postRoutes.delete('/:postID/comments/:id', isAuth, deleteComment);
+
+// Routes for upvote/ downvote
+postRoutes.post('/:postID/vote', isAuth, vote);
 
 export default postRoutes;
